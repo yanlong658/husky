669de11c45f8959b21f3235a98d71a91bb03dc3d
EE405423 <yanlong658@gmail.com>
2020-04-08 19:33:08 +0800

    update two way

--- src/husky_path.cpp
+++ src/husky_path.cpp
@@ -27,15 +27,10 @@ geometry_msgs::Point acc;
 ncrl_tf::Trans goal_pose, world2body, lio_pose;
 tf::Transform world;
 
-//lio
-//lio_path show trajectory on body frame
-nav_msgs::Path lio_path;
 
 //global variable
 int i = 0;
-int count_;
-int ENABLE_FEEDFORWARD = 0;
-bool systemInit = false;
+int count_ = 0;
 double init_time;
 
 
@@ -88,273 +83,270 @@ void cb_pos(const nav_msgs::Odometry::ConstPtr& msg){
 
 void process()
 {
-    //ncrl::tf
-    static tf::TransformBroadcaster br;
-    ros::Rate r(freq);
+  //ncrl::tf
+  static tf::TransformBroadcaster br;
+  ros::Rate r(freq);
 
-    //要傳給husky的vel_cmd指令
-    geometry_msgs::Twist vel_cmd;
-    int flag = 1;
-    while(ros::ok())
+  //要傳給husky的vel_cmd指令
+  geometry_msgs::Twist vel_cmd;
+  int flag = 1;
+  while(ros::ok())
+  {
+    int c = getch();
+    if (c != EOF)
     {
-        int c = getch();
-        if (c != EOF)
-        {
-          switch(c)
+      switch(c)
+      {
+        case 49:     // key 1
+          flag = 1;
+        break;
+        case 50:     // key 2
+          flag = 2;
+        break;
+        case 51:     // key 3
+          flag = 3;
+        break;
+      }
+    }
+
+    else if (flag == 2 )
+    {
+      ROS_INFO(" ===== enter ===== ");
+      double max;
+      double sample=0.03;
+      qptrajectory plan;
+      path_def path;
+      trajectory_profile p1,p2,p3,p4,p5,p6;
+
+      std::vector<trajectory_profile> data;
+      p1.pos << 0.0,0,0;
+      p1.vel << 0.0,0.0,0;
+      p1.acc << 0.00,-0.0,0;
+      p1.yaw = 0;
+
+      p2.pos<< 3.0,0.0,0;
+      p2.vel<< 0,0,0;
+      p2.acc<< 0,0,0;
+      p2.yaw = 0;
+
+      p3.pos<< 4.0,-1.0,0.0;
+      p3.vel<< 0,0,0;
+      p3.acc<< 0,0,0;
+      p3.yaw = 0;
+
+      p4.pos << 4.0,-2.5,0;
+      p4.vel << 0,0,0;
+      p4.acc << 0,0,0;
+      p4.yaw = 0;
+
+      p5.pos << 3.0,-3.5,0;
+      p5.vel << 0.0,0.0,0;
+      p5.acc << 0.00,-0.0,0;
+      p5.yaw = 0;
+
+      p6.pos << 0.0,-3.5,0;
+      p6.vel << 0.0,0.0,0;
+      p6.acc << 0.00,-0.0,0;
+      p6.yaw = 0;
+
+      path.push_back(segments(p1,p2,4));
+      path.push_back(segments(p2,p3,2));
+      path.push_back(segments(p3,p4,2));
+      path.push_back(segments(p4,p5,2));
+      path.push_back(segments(p5,p6,4));
+
+      data = plan.get_profile(path ,path.size(),sample);
+      max = data.size();
+
+      while(ros::ok())
+      {
+          if(count_ >=max)
           {
-            case 49:     // key 1
-              flag = 1;
-            break;
-            case 50:     // key 2
-              flag = 2;
-            break;
-            case 51:     // key 3
-              flag = 3;
-            break;
-            case 52:     // key 4
-              ENABLE_FEEDFORWARD = 1 - ENABLE_FEEDFORWARD;
-            break;
-            case 119:    // key w
-              vel_cmd.linear.x += 0.1;
-            break;
-            case 115:    // key s
-              vel_cmd.linear.x += -0.1;
-            break;
-            case 100:    // key d
-              vel_cmd.angular.z += -0.1;
-            break;
-            case 97:    // key a
-              vel_cmd.angular.z += 0.1;
-            break;
-            case 114:    // key r
-              vel_cmd.linear.x = 0;
-              vel_cmd.angular.z = 0;
-            break;
-            case 105:    // key i
-            goal_pose.v(0) += 0.1;
-            break;
-            case 106:    // key j
-            goal_pose.v(1) += 0.1;
-            break;
-            case 107:    // key k
-            goal_pose.v(0) -= 0.1;
-            break;
-            case 108:    // key l
-            goal_pose.v(1) -= 0.1;
+
+            vel_cmd.linear.x = 0;
+            vel_cmd.angular.z = 0;
+
+            //讓車子停在定點
+            pub_cmd.publish(vel_cmd);
+            flag = 1;
+            count_ = 0;
             break;
           }
-        }
-        if (flag == 1)
-        {
-            ROS_INFO(" ===== KEYBOARD CONTROL ===== ");
-            ENABLE_FEEDFORWARD = 0;
-            goal_pose.v = lio_pose.v;
-            goal_pose.q = lio_pose.q;
-        }
-        else if (flag == 2)
-        {
-            ROS_INFO(" ===== TRACE ===== ");
+
+          else
+          {
+            goal_path.header.stamp= ros::Time::now();
+            goal_path.header.frame_id="WORLD";
+
+            geometry_msgs::PoseStamped this_pose_stamped;
+
+            this_pose_stamped.header.stamp=ros::Time::now();
+            this_pose_stamped.header.frame_id="WORLD";
+
+            this_pose_stamped.pose.position.x = data[count_].pos[0];
+            this_pose_stamped.pose.position.y = data[count_].pos[1];
+
+            goal_path.poses.push_back(this_pose_stamped);
+
+            goal_pose.v << data[count_].pos[0] ,data[count_].pos[1] , data[count_].pos[2];
+            vel.x = data[count_].vel[0];
+
+            count_ += 1 ;
+
+            //</lio_path>
+            path_pub.publish(goal_path);
         }
 
-        if (ENABLE_FEEDFORWARD)
-            ROS_INFO(" ===== ENABLE FEEDFORWARD TERM =====");
+          Eigen::Vector3d error,error_last;
 
-        if (flag == 1)
+          error= goal_pose.v - lio_pose.v;
+
+          float cmd_x, cmd_y;
+          pid_compute(pid_x, cmd_x, error(0), error_last(0), 0.001);
+          pid_compute(pid_y, cmd_y, error(1), error_last(1), 0.001);
+
+          error_last = error;
+
+          Eigen::Vector3d cmd(cmd_x, cmd_y, 0);
+
+          //trans imu_init frame cmd into body frame
+          cmd = lio_pose.q.inverse() * cmd;
+
+          //feedforward
+          vel_cmd.linear.x = cmd(0);
+          vel_cmd.angular.z = cmd(1);
+          //vel_cmd.linear.x += data[count_].vel[0];
+
+          if (fabs(vel_cmd.linear.x) > maxCmdX)
+          {
+              vel_cmd.linear.x = vel_cmd.linear.x * maxCmdX / fabs(vel_cmd.linear.x);
+          }
+          else if (fabs(vel_cmd.linear.x) < minCmdX)
+          {
+              vel_cmd.linear.x = 0;
+          }
+
+          if (fabs(vel_cmd.angular.z) > maxCmdW)
+          {
+              vel_cmd.angular.z = vel_cmd.angular.z * maxCmdW / fabs(vel_cmd.angular.z);
+          }
+          else if (fabs(vel_cmd.angular.z) < minCmdW)
+          {
+          vel_cmd.angular.z = 0;
+          }
+
+          pub_cmd.publish(vel_cmd);
+          r.sleep();
+        }
+      }
+
+    else if (flag == 3 )
+    {
+      ROS_INFO(" ===== enter ===== ");
+      double max_ = 0;
+      double sample_=0.03;
+      qptrajectory plan_;
+      path_def path_;
+      trajectory_profile p1_,p2_;
+
+      std::vector<trajectory_profile> data_;
+      p1_.pos << 0.0,-3.5,0;
+      p1_.vel << 0.0,0.0,0;
+      p1_.acc << 0.00,-0.0,0;
+      p1_.yaw = 0;
+
+      p2_.pos<< 0.0,0.0,0;
+      p2_.vel<< 0,0,0;
+      p2_.acc<< 0,0,0;
+      p2_.yaw = 0;;
+
+      path_.push_back(segments(p1_,p2_,8));
+
+
+      data_ = plan_.get_profile(path_ ,path_.size(),sample_);
+      max_ = data_.size();
+
+      while(ros::ok())
+      {
+          if(count_ >= max_)
+          {
+
+            vel_cmd.linear.x = 0;
+            vel_cmd.angular.z = 0;
+
+            //讓車子停在定點
             pub_cmd.publish(vel_cmd);
+            count_ = 0;
+            flag = 1;
+            break;
+          }
+         else
+          {
+            goal_path.header.stamp= ros::Time::now();
+            goal_path.header.frame_id="WORLD";
 
-        // choose suitable point
-        if (flag == 2)
-        {
-            ROS_INFO(" ===== enter ===== ");
-            double max;
-            double sample=0.02;
-            qptrajectory plan;
-            path_def path;
-            trajectory_profile p1,p2,p3,p4,p5,p6;
+            geometry_msgs::PoseStamped this_pose_stamped;
 
-            std::vector<trajectory_profile> data;
-            p1.pos << 0.0,0,0;
-            p1.vel << 0.0,0.0,0;
-            p1.acc << 0.00,-0.0,0;
-            p1.yaw = 0;
+            this_pose_stamped.header.stamp=ros::Time::now();
+            this_pose_stamped.header.frame_id="WORLD";
 
-            p2.pos<< 6.0,0.0,0;
-            p2.vel<< 0,0,0;
-            p2.acc<< 0,0,0;
-            p2.yaw = 0;
+            this_pose_stamped.pose.position.x = data_[count_].pos[0];
+            this_pose_stamped.pose.position.y = data_[count_].pos[1];
 
-            p3.pos<< 8.0,2.0,0.0;
-            p3.vel<< 0,0,0;
-            p3.acc<< 0,0,0;
-            p3.yaw = 0;
+            goal_path.poses.push_back(this_pose_stamped);
 
-            p4.pos << 8.0,5.0,0;
-            p4.vel << 0,0,0;
-            p4.acc << 0,0,0;
-            p4.yaw = 0;
+            goal_pose.v << data_[count_].pos[0] ,data_[count_].pos[1] , data_[count_].pos[2];
+            vel.x = data_[count_].vel[0];
 
-            p5.pos << 6.0,7.0,0;
-            p5.vel << 0.0,0.0,0;
-            p5.acc << 0.00,-0.0,0;
-            p5.yaw = 0;
+            count_ += 1 ;
 
-            p6.pos << 0.0,7.0,0;
-            p6.vel << 0.0,0.0,0;
-            p6.acc << 0.00,-0.0,0;
-            p6.yaw = 0;
+            //</lio_path>
+            path_pub.publish(goal_path);
+          }
 
-            path.push_back(segments(p1,p2,8));
-            path.push_back(segments(p2,p3,4));
-            path.push_back(segments(p3,p4,4));
-            path.push_back(segments(p4,p5,4));
-            path.push_back(segments(p5,p6,8));
-            data = plan.get_profile(path ,path.size(),sample);
-            max = data.size();
-
-            std::cout<<"ros::ok-------------"<<ros::ok<<std::endl;
-            std::cout<<"max"<<max<<std::endl;
-
-            while(ros::ok())
-            {
-                if(count_ >=max)
-                {
-                    world2body.v << 0,0,0;
-                    Eigen::Vector3d euler(0,0,0);
-                    world2body.q = ncrl_tf::Euler2Q(euler);
-
-                    vel.x = 0;
-                    vel.y = 0;
-                    vel.z = 0;
-
-                    acc.x = 0;
-                    acc.y = 0;
-                    acc.z = 0;
-                    count_ =0;
-
-                    vel_cmd.linear.x = 0;
-                    vel_cmd.angular.z = 0;
-
-                    std::cout<<"break---------------------------"<<std::endl;
-		    //讓車子停在定點
-                    pub_cmd.publish(vel_cmd);
-                    flag = 1;
-                    break;
-                }
-
-                else
-                {
-
-                std::cout<<"done1"<<std::endl;
-
-                ros::Time current_time;
-                current_time = ros::Time::now();
-
-                goal_path.header.stamp=current_time;
-                goal_path.header.frame_id="WORLD";
-
-                geometry_msgs::PoseStamped this_pose_stamped;
-
-                this_pose_stamped.header.stamp=current_time;
-                this_pose_stamped.header.frame_id="WORLD";
-
-                this_pose_stamped.pose.position.x = data[count_].pos[0];
-                this_pose_stamped.pose.position.y = data[count_].pos[1];
-
-                goal_path.poses.push_back(this_pose_stamped);
-		
-		//將lio的world frame轉到車子的body frame
-                world2body.v = world2body.q.inverse()* lio_pose.v + world2body.v;
-                world2body.q = world2body.q * lio_pose.q;
-
-                ncrl_tf::setTfTrans(world , world2body.q ,world2body.v);
-                br.sendTransform(tf::StampedTransform(world, current_time ,"WORLD","now"));
-
-                goal_pose.v << data[count_].pos[0] ,data[count_].pos[1] , data[count_].pos[2];
-
-                vel.x = data[count_].vel[0];
-                vel.y = data[count_].vel[1];
-                vel.z = data[count_].vel[2];
-
-                acc.x = data[count_].acc[0];
-                acc.y = data[count_].acc[1];
-                acc.z = data[count_].acc[2];
-                count_ += 1 ;
-
-                //<lio_path>
-                lio_path.header.stamp=current_time;
-                lio_path.header.frame_id="WORLD";
-
-                geometry_msgs::PoseStamped lio_pose_stamped;
-
-                lio_pose_stamped.header.stamp=current_time;
-                lio_pose_stamped.header.frame_id="WORLD";
-
-                lio_pose_stamped.pose.position.x = world2body.v(0);
-                lio_pose_stamped.pose.position.y = world2body.v(1);
-
-                lio_path.poses.push_back(lio_pose_stamped);
-
-                //</lio_path>
-
-                path_pub.publish(goal_path);
-                lio_pub_path.publish(lio_path);
+          Eigen::Vector3d error,error_last;
 
 
-                }
+          error= goal_pose.v - lio_pose.v;
 
-                Eigen::Vector3d error,error_last;
+          float cmd_x, cmd_y;
+          pid_compute(pid_x, cmd_x, error(0), error_last(0), 0.001);
+          pid_compute(pid_y, cmd_y, error(1), error_last(1), 0.001);
 
+          error_last = error;
 
-                for(int i=0; i<max;i++)
-                {
-                    if(i ==0)
-                    {
-                        world2body.v << 0, 0, 0;
-                    }
+          Eigen::Vector3d cmd(cmd_x, cmd_y, 0);
 
-                    else
-                    {
+          //trans imu_init frame cmd into body frame
+          cmd = lio_pose.q.inverse() * cmd;
 
-                        goal_pose.v = world2body.q * goal_pose.v + world2body.v;
+          //feedforward
+          vel_cmd.linear.x = cmd(0);
+          vel_cmd.angular.z = cmd(1);
+          //vel_cmd.linear.x += data[count_].vel[0];
 
-                        error= goal_pose.v - lio_pose.v;
-                        error_last = error;
+          if (fabs(vel_cmd.linear.x) > maxCmdX)
+          {
+              vel_cmd.linear.x = vel_cmd.linear.x * maxCmdX / fabs(vel_cmd.linear.x);
+          }
+          else if (fabs(vel_cmd.linear.x) < minCmdX)
+          {
+              vel_cmd.linear.x = 0;
+          }
 
-                        float cmd_x, cmd_y;
-                        pid_compute(pid_x, cmd_x, error(0), error_last(0), 0.001);
-                        pid_compute(pid_y, cmd_y, error(1), error_last(1), 0.001);
-
-                        std::cout <<"error_x" <<error <<std::endl;
-
-                        Eigen::Vector3d cmd(cmd_x, cmd_y, 0);
-
-                        //trans imu_init frame cmd into body frame
-                        cmd = lio_pose.q.inverse() * cmd;
-
-                        vel_cmd.linear.x = cmd(0);
-                        vel_cmd.angular.z = cmd(1);
-
-                        if (fabs(vel_cmd.linear.x) > maxCmdX){
-                            vel_cmd.linear.x = vel_cmd.linear.x * maxCmdX / fabs(vel_cmd.linear.x);
-                        } else if (fabs(vel_cmd.linear.x) < minCmdX){
-                            vel_cmd.linear.x = 0;
-                        }
-
-                        //std::cout <<"vel_cmd.linear.x" <<vel_cmd.linear.x <<std::endl;
-
-                        if (fabs(vel_cmd.angular.z) > maxCmdW){
-                            vel_cmd.angular.z = vel_cmd.angular.z * maxCmdW / fabs(vel_cmd.angular.z);
-                        } else if (fabs(vel_cmd.angular.z) < minCmdW){
-                          vel_cmd.angular.z = 0;
-                        }
-
-                        pub_cmd.publish(vel_cmd);
-                    }
-                }
-                r.sleep();
-            }
+          if (fabs(vel_cmd.angular.z) > maxCmdW)
+          {
+              vel_cmd.angular.z = vel_cmd.angular.z * maxCmdW / fabs(vel_cmd.angular.z);
+          }
+          else if (fabs(vel_cmd.angular.z) < minCmdW)
+          {
+          vel_cmd.angular.z = 0;
+          }
+          pub_cmd.publish(vel_cmd);
+          r.sleep();
         }
-    }
+      }
+  }
 }
 
 
@@ -388,8 +380,6 @@ int main(int argc, char **argv)
     path_pub = nh.advertise<nav_msgs::Path>("trajectory",1, true);
     pub_cmd = nh.advertise<geometry_msgs::Twist> ("/cmd_vel", 100);
 
-    //lio的路徑
-    lio_pub_path = nh.advertise<nav_msgs::Path> ("/future_path", 20);
 
     //init setting
     ncrl_tf::setTransFrame(lio_pose, "WORLD", "IMU");
@@ -397,7 +387,6 @@ int main(int argc, char **argv)
     //process have no ()
     std::thread ctrl_process{process};
 
-    std::cout<<"rrrrrrrrrrrrrrrrrrr"<<std::endl;
     ros::spin();
     return 0;
 }
